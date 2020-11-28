f64fd46029456c84a440f267081d145febafc438
John Schmidt <john@john2143.com>
2020-11-27 22:39:24 -0500

    update to bevy 3 and use new camera system

--- src/main.rs
+++ src/main.rs
@@ -1,207 +1,206 @@
-use bevy::prelude::{
-    shape,
-    App,
-    AppBuilder,
-    Commands,
-    Input,
-    IntoQuerySystem,
-    KeyCode,
-    Query,
-    //IntoForEachSystem,
-    //IntoThreadLocalSystem,
-    Res,
-    ResMut,
-    Time,
-    Vec3,
-};
-
-use bevy_pbr::prelude::*;
-use bevy_transform::prelude::Translation;
-
-use bevy_render::prelude::{Color, Mesh, Msaa};
-
-use bevy_asset::Assets;
-
-use bevy::prelude::*;
-
-mod camera;
-use camera::{FlyCamera, FlyCameraPlugin};
-
-fn default_plugins() -> AppBuilder {
-    let mut app = App::build();
-
-    app.add_resource(Msaa { samples: 4 });
-    app.add_plugin(bevy_type_registry::TypeRegistryPlugin::default());
-    app.add_plugin(bevy_core::CorePlugin::default());
-    app.add_plugin(bevy_transform::TransformPlugin::default());
-    app.add_plugin(bevy_diagnostic::DiagnosticsPlugin::default());
-    app.add_plugin(bevy_input::InputPlugin::default());
-    app.add_plugin(bevy_window::WindowPlugin::default());
-    app.add_plugin(bevy_asset::AssetPlugin::default());
-    app.add_plugin(bevy_scene::ScenePlugin::default());
-    app.add_plugin(bevy_render::RenderPlugin::default());
-    //app.add_plugin(bevy_sprite::SpritePlugin::default());
-    app.add_plugin(bevy_pbr::PbrPlugin::default());
-    //app.add_plugin(bevy_ui::UiPlugin::default());
-    //app.add_plugin(bevy_text::TextPlugin::default());
-
-    //app.add_plugin(bevy_audio::AudioPlugin::default());
-    //app.add_plugin(bevy_gltf::GltfPlugin::default());
-    app.add_plugin(bevy_winit::WinitPlugin::default());
-    app.add_plugin(bevy_wgpu::WgpuPlugin::default());
-
-    app
-}
+use bevy::{input::mouse::MouseMotion, prelude::*};
 
 fn main() {
-    default_plugins()
-        .add_plugin(FlyCameraPlugin)
+    App::build()
+        //.add_resource(Msaa { samples: 4 })
+        .init_resource::<State>()
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
-        .add_system(control_objects.system())
-        .add_system(global_keys.system())
+        .add_system(update_player.system())
         .run();
 }
 
-fn global_keys(
-    //events: Res<Events<>>,
-    input: Res<Input<KeyCode>>,
-) {
-    if input.pressed(KeyCode::Escape) {}
+//based on Bevy-WoW camera
+struct CameraOrientation {
+    yaw: f32,
+    pitch: f32,
+    roll: f32,
+
+    distance: f32,
+
+    attached_entity: Option<Entity>,
 }
 
-struct Control(bool);
-
-struct Phys {
-    velocity: Vec3,
+#[derive(Default)]
+struct State {
+    mouse_motion_event_reader: EventReader<MouseMotion>,
 }
 
-impl Default for Phys {
+impl Default for CameraOrientation {
     fn default() -> Self {
         Self {
-            velocity: Default::default(),
+            yaw: 0.,
+            pitch: 0.,
+            roll: 0.,
+            distance: 100.,
+            attached_entity: None,
         }
     }
 }
 
-fn is_on_ground(v: &Vec3) -> bool {
-    v.y() <= 0.0
-}
+struct PlayerCamera;
 
-fn set_clamped<T: std::cmp::PartialOrd>(val: T, min: T, max: T) -> T {
-    if val < min {
-        min
-    } else if val > max {
-        max
-    } else {
-        val
-    }
-}
-
-fn control_objects(
-    time: Res<Time>,
-    input: Res<Input<KeyCode>>,
-    mut camera_query: Query<(&camera::ModifiedFlyCameraOptions, &Rotation)>,
-    mut query: Query<(&Control, &mut Translation, &mut Phys)>,
-) {
-    let mut cam: Rotation = Rotation::identity();
-
-    for (_options, rotation) in &mut camera_query.iter() {
-        cam.clone_from(&rotation);
-        break;
-    }
-
-    for (control, mut translation, mut phys) in &mut query.iter() {
-        if !control.0 {
-            continue;
-        }
-
-        let grounded = is_on_ground(&translation.0);
-
-        //gravity
-        *phys.velocity.y_mut() -= 20.0 * time.delta_seconds;
-
-        if grounded {
-            if phys.velocity.y() < 0.0 {
-                *phys.velocity.y_mut() = 0.0;
-            }
-            *translation.y_mut() = 0.0;
-
-            if input.pressed(KeyCode::Space) {
-                *phys.velocity.y_mut() += 8.0;
-            }
-        }
-
-        use camera::{forward_walk_vector, movement_axis, strafe_vector};
-
-        let axis_h = movement_axis(&input, KeyCode::D, KeyCode::A);
-        let axis_v = movement_axis(&input, KeyCode::S, KeyCode::W);
-
-        let delta_f = forward_walk_vector(&cam) * axis_v * 120.0 * time.delta_seconds;
-        let delta_strafe = strafe_vector(&cam) * axis_h * 120.0 * time.delta_seconds;
-
-        phys.velocity += delta_f + delta_strafe;
-
-        let xz_move = Vec3::new(phys.velocity.x(), 0.0, phys.velocity.z());
-        if xz_move.length_squared() > 0.01 {
-            phys.velocity -= xz_move.normalize() * 0.5 * time.delta_seconds;
-            dbg!(phys.velocity);
-            dbg!(xz_move.normalize());
-        }
-
-        translation.0 += phys.velocity * time.delta_seconds;
-    }
-}
-
+/// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let cube_size = 2.0;
-    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: cube_size }));
-    let cube_material_handle = materials.add(StandardMaterial {
-        albedo: Color::rgb(0.5, 0.4, 0.3),
+    let player_material = materials.add({
+        let mut q = StandardMaterial::from(Color::rgb(1.0, 0.5, 0.0));
+        q.shaded = true;
+        q
+    });
+
+    // add entities to the world
+    let e = commands
+        .spawn(Camera3dComponents::default())
+        .with(PlayerCamera)
+        .current_entity();
+
+    let player = commands
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.9 })),
+            transform: Transform {
+                translation: Vec3::new(0.0, 1.9, 0.0),
+                ..Default::default()
+            },
+            material: player_material.clone(),
+            ..Default::default()
+        })
+        .with(CameraOrientation {
+            attached_entity: e,
+            ..Default::default()
+        })
+        .current_entity();
+
+    commands.push_children(player.unwrap(), &[e.unwrap()]);
+    commands.spawn(LightComponents::default());
+    commands.spawn(PbrComponents {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 300.0 })),
+        material: materials.add(Color::rgb(0.1, 0.5, 0.7).into()),
         ..Default::default()
     });
 
-    commands
-        .spawn(PbrComponents {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 10000.0 })),
-            material: materials.add(Color::rgb(0.1, 0.2, 0.1).into()),
-            translation: Translation::new(0.0, -cube_size, 0.0),
-            ..Default::default()
-        })
-        .spawn(LightComponents {
-            translation: Translation::new(4.0, 5.0, -4.0),
-            ..Default::default()
-        })
-        .spawn(LightComponents {
-            translation: Translation::new(4.0, 5.0, 4.0),
-            ..Default::default()
-        })
-        // camera
-        //.spawn(Camera3dComponents {
-        //transform: Transform::new_sync_disabled(Mat4::face_toward(
-        //Vec3::new(5.0, 10.0, 10.0),
-        //Vec3::new(0.0, 0.0, 0.0),
-        //Vec3::new(0.0, 1.0, 0.0),
-        //)),
-        //..Default::default()
-        //});
-        .spawn(FlyCamera {
-            translation: Translation::new(10.0, 5.0, 4.0),
-            rotation: Rotation::from_rotation_x(50.0),
+    let cubes = [
+        (5.0, 1.0, 5.0),
+        (25.0, 1.0, 45.0),
+        (-20.0, 1.0, 0.0),
+    ];
+    for cube in &cubes{
+        commands.spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
+            transform: Transform {
+                translation: Vec3::new(cube.0, cube.1, cube.2),
+                ..Default::default()
+            },
             ..Default::default()
         });
-        //.spawn(FlyCamera::default());
+    }
+}
 
-    commands
-        .spawn(PbrComponents {
-            mesh: cube_handle,
-            material: cube_material_handle,
-            translation: Translation::new(0.0, 2.0, 0.0),
-            ..Default::default()
-        })
-        .with(Control(true))
-        .with(Phys::default());
+fn mouse(
+    time: Res<Time>,
+    mut state: ResMut<State>,
+    mouse_motion: Res<Events<MouseMotion>>,
+    mut query: Query<&mut CameraOrientation>,
+) {
+}
+
+trait RemappableFloat {}
+impl RemappableFloat for f64 {}
+impl RemappableFloat for f32 {}
+
+fn remap<T>(src: T, (src_min, src_max): (T, T), (dest_min, dest_max): (T, T)) -> T
+where
+    T: RemappableFloat
+        + Copy
+        + std::ops::Sub<Output = T>
+        + std::cmp::PartialOrd
+        + std::ops::Mul<Output = T>
+        + std::ops::Div<Output = T>
+        + std::ops::Add<Output = T>,
+{
+    if src < src_min {
+        return dest_min;
+    } else if src > src_max {
+        return dest_max;
+    }
+
+    let off = src - src_min;
+    let off_pct = off / (src_max - src_min);
+
+    let dest_range = dest_max - dest_min;
+
+    dest_min + dest_range * off_pct
+}
+
+fn update_player(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<(&mut CameraOrientation, &mut Transform)>,
+    mut camera_query: Query<(&mut PlayerCamera, Entity, &mut Transform)>,
+) {
+    let mut movement = Vec2::zero();
+    if keyboard_input.pressed(KeyCode::E) {
+        *movement.y_mut() += 1.;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        *movement.y_mut() -= 1.;
+    }
+    if keyboard_input.pressed(KeyCode::F) {
+        *movement.x_mut() += 1.;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        *movement.x_mut() -= 1.;
+    }
+
+    if movement != Vec2::zero() {
+        movement.normalize();
+    }
+
+    let move_speed = 10.0;
+    movement *= time.delta_seconds * move_speed;
+
+    for (mut player, mut transform) in player_query.iter_mut() {
+        player.pitch = player.pitch.max(1f32.to_radians()).min(179f32.to_radians());
+        player.distance = player.distance.max(5.).min(30.);
+
+        //transform.translation += Vec3::new(0.0, 1.0, 0.0) * time.delta_seconds;
+
+        player.pitch = remap(
+            (time.seconds_since_startup * 0.2).sin(),
+            (-1.0, 1.0),
+            (25.0f64.to_radians(), 90.0f64.to_radians()),
+        ) as f32;
+
+        player.yaw = remap(
+            (time.seconds_since_startup * 0.3).cos(),
+            (-1.0, 1.0),
+            (-180.0f64.to_radians(), 180.0f64.to_radians()),
+        ) as f32;
+
+        if player.pitch > 179f32.to_radians() {
+            player.pitch -= 179f32.to_radians()
+        }
+
+        //println!("{} {}", player.yaw, player.pitch);
+
+        transform.translation += Vec3::new(0.0, 0.0, 0.0);
+        transform.rotation = Quat::from_rotation_y(-player.yaw);
+
+        if let Some(camera_entity) = player.attached_entity {
+            let cam_pos = Vec3::new(0., player.pitch.cos(), -player.pitch.sin()).normalize()
+                * player.distance;
+            for (_, e, mut camera3dtrans) in camera_query.iter_mut() {
+                if camera_entity != e {
+                    continue;
+                }
+                camera3dtrans.translation = cam_pos;
+                let look = Mat4::face_toward(cam_pos, Vec3::zero(), Vec3::new(0.0, 1.0, 0.0));
+                camera3dtrans.rotation = look.to_scale_rotation_translation().1;
+            }
+        }
+    }
 }
